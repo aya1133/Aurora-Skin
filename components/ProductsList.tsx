f51562c88a6625f7446b1fa88aebdd2335cf2d62
aya <iaya88100@gmail.com>
2025-07-12 16:59:38 -0700

    edite margen

--- components/ProductsList.tsx
+++ components/ProductsList.tsx
@@ -29,7 +29,7 @@ return(
             placeholder="Serach products..."
             className="w-full max-w-md rounded-full border border-gray-300 px-4 py-2 !p-2 focus:outline-none focus:ring-2 focus:ring-pink-500 !mt-10 !mb-10"/>
         </div>
-        <ul className="mt-6 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 !ml-20">
+        <ul className="mt-6 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 !ml-10">
            
             {filteredProduct.map((product, key) => {
                 return <li key={key}> 
